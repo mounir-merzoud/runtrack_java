c4dc2ac3ede5a873996ae5701aec6c6674d68b9e
mounir-merzoud <mounir.merzoud@laplateforme.io>
2024-07-02 20:12:46 +0200

    Jour02

---
+++ Jour02/Job01/NumbersFrom1To100.java
@@ -0,0 +1,11 @@
+package Job01; // Définition du package Job01
+
+public class NumbersFrom1To100 {
+    public static void main(String[] args) {
+        // Utilisation d'une boucle for pour afficher les chiffres de 1 à 100
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i); // Affiche chaque chiffre sur une nouvelle ligne
+        }
+    }
+}
+
---
+++ Jour02/Job02/NumbersFrom100To1.java
@@ -0,0 +1,10 @@
+package Job02; // Définition du package Job02
+
+public class NumbersFrom100To1 {
+    public static void main(String[] args) {
+        // Utilisation d'une boucle for pour afficher les chiffres de 100 à 1
+        for (int i = 101; i >= 1; i--) {
+            System.out.println(i); // Affiche chaque chiffre sur une nouvelle ligne
+        }
+    }
+}
